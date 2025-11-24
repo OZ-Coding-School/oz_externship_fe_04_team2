30cc68be7b1b378fb87efce90bcb3af94c330887
Jay <jiyeyoun@outlook.com>
2025-11-24 18:39:32 +0900

    feat: SVG 파일을 React 컴포넌트로 사용할 수 있도록 svgr 적용 (#25)

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -1,12 +1,12 @@
-import ImageLogoIcon from '@/assets/ImageLogo.svg'
-import TextLogoIcon from '@/assets/TextLogo.svg'
+import ImageLogoIcon from '@/assets/ImageLogo.svg?react'
+import TextLogoIcon from '@/assets/TextLogo.svg?react'
 
 export function HeaderLogo() {
   return (
-    <div>
-      <ImageLogoIcon />
+    <div className="flex items-center gap-2">
+      <ImageLogoIcon className="h-7 w-7" />
       <div className="hidden md:flex">
-        <TextLogoIcon />
+        <TextLogoIcon className="mt-1 w-20" />
       </div>
     </div>
   )
