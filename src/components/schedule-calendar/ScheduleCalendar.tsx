a5f22b09bf09fb8aac709c36124c623ba3fbbc71
hyebinpark <hoonywhitesocks@gmail.com>
2025-11-25 18:00:02 +0900

    style: react-big-calendar 기본 스타일 오버라이딩 (#27)

--- src/components/schedule-calendar/ScheduleCalendar.tsx
+++ src/components/schedule-calendar/ScheduleCalendar.tsx
@@ -1,15 +1,16 @@
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import '../../calendar.css'
 import Toolbar from '@/components/schedule-calendar/CustomToolbar'
 import { localizer } from '@/utils'
 import { useState } from 'react'
 import { Calendar } from 'react-big-calendar'
-import 'react-big-calendar/lib/css/react-big-calendar.css'
 
 export default function ScheduleCalendar() {
   const [month, setMonth] = useState(new Date())
   const formats = { monthHeaderFormat: 'yyyy년 MM월' }
 
   return (
-    <div className="schedule-calendar h-[600px]">
+    <div className="h-[600px]">
       <Calendar
         localizer={localizer}
         date={month}
