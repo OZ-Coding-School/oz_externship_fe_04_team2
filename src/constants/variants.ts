ee850f3bdb8a876a5faea63c7d6e1b28aee459bf
Jay <jiyeyoun@outlook.com>
2025-11-26 11:48:47 +0900

    style: 버튼 커서를 포인터로 변경 (#36)

--- src/constants/variants.ts
+++ src/constants/variants.ts
@@ -1,7 +1,7 @@
 import { cva } from 'class-variance-authority'
 
 export const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-lg text-sm font-medium transition-colors focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 ring-offset-white',
+  'inline-flex items-center justify-center whitespace-nowrap rounded-lg text-sm font-medium transition-colors focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 ring-offset-white cursor-pointer',
   {
     variants: {
       variant: {
