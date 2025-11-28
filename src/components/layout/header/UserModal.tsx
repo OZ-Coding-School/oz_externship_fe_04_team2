49a921852b996ffb95cc1285683874a3895a0d14
Jay <jiyeyoun@outlook.com>
2025-11-28 15:52:38 +0900

    refactor: UserModal 색상 입력 방식 변경

--- src/components/layout/header/UserModal.tsx
+++ src/components/layout/header/UserModal.tsx
@@ -2,7 +2,7 @@ import { LogOutIcon, UserRound } from 'lucide-react'
 
 export function UserModal() {
   return (
-    <div className="hidden md:absolute md:top-[45.05px] md:right-2.5 md:flex md:h-24 md:w-48 md:flex-col md:gap-1 md:rounded-lg md:border md:border-solid md:border-[#E5E7EB] md:bg-white md:drop-shadow-[0_10px_15px_rgba(0,0,0,0.2)]">
+    <div className="md:border-custom-gray-200 hidden md:absolute md:top-[45.05px] md:right-2.5 md:flex md:h-24 md:w-48 md:flex-col md:gap-1 md:rounded-lg md:border md:border-solid md:bg-white md:drop-shadow-[0_10px_15px_rgba(0,0,0,0.2)]">
       <div className="text-custom-gray-700 mt-3 flex cursor-pointer items-center gap-3 px-4 py-1.5 pt-[5px] text-[16px]">
         <LogOutIcon className="h-4 w-4" />
         <span>로그아웃</span>
