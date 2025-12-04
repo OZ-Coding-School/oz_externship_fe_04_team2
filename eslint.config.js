fe0639fcbd4f05acbaa964e322237cebed27759b
Jay <jiyeyoun@outlook.com>
2025-12-04 10:46:05 +0900

    chore: console.error는 허용하도록 eslint 설정 변경

--- eslint.config.js
+++ eslint.config.js
@@ -99,7 +99,7 @@ export default tseslint.config(
       'react/jsx-no-useless-fragment': 'warn', // 불필요한 Fragment 경고
 
       // ===== 기본적인 코드 품질 =====
-      'no-console': 'warn', // console.log 사용시 경고 (개발 중에는 필요할 수 있음)
+      'no-console': ['error', { allow: ['warn', 'error'] }], // console.log 사용시 경고 (개발 중에는 필요할 수 있음)
       'no-debugger': 'warn', // debugger 문 사용시 경고 (에러 아님)
       'no-duplicate-imports': 'warn', // 중복 import 경고
 
