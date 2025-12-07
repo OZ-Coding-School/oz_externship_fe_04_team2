4a781b4f8461372ee3ac4511f7782e1d24327dbd
hyebinpark <hoonywhitesocks@gmail.com>
2025-12-08 02:00:21 +0900

    feat: 스터디 기록 작성 페이지 기본 UI 구현 (#103)

--- src/pages/CreateStudyNotePage.tsx
+++ src/pages/CreateStudyNotePage.tsx
@@ -1,9 +1,47 @@
+import { Button, ImageUploader, Input } from '@/components/common'
+import { MarkdownEditor } from '@/components/markdown'
 import { StudyNoteBreadcrumb } from '@/components/studygroup-note'
 
 export function CreateStudyNotePage() {
   return (
-    <div>
+    <div className="flex flex-col p-8">
       <StudyNoteBreadcrumb />
+      <header className="pt-2 pb-6">
+        <h1 className="text-custom-gray-900 text-3xl font-bold">
+          스터디 기록 작성
+        </h1>
+        <p className="text-custom-gray-600 pt-2">
+          학습한 내용을 자세히 기록해보세요.
+        </p>
+      </header>
+
+      <div className="border-custom-gray-200 flex flex-col gap-6 rounded-xl border p-6">
+        <Input
+          label="제목"
+          placeholder="스터디 기록의 제목을 입력하세요."
+          required
+        />
+        <div>
+          <label className="text-custom-gray-700 text-sm font-medium">
+            내용 <span className="text-red-500">*</span>
+          </label>
+          <MarkdownEditor />
+        </div>
+
+        <div>
+          <label className="text-custom-gray-700 text-sm font-medium">
+            첨부 파일
+          </label>
+          <ImageUploader />
+        </div>
+      </div>
+
+      <div className="flex w-full justify-between gap-4 pt-6">
+        <Button variant="outline">취소</Button>
+        <Button variant="primary" className="px-8">
+          기록 저장
+        </Button>
+      </div>
     </div>
   )
 }
