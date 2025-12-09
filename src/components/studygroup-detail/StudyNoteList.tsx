d8e655743cb02f47c2e0dd3c29d618d088b98a2f
hyebinpark <hoonywhitesocks@gmail.com>
2025-12-09 14:20:39 +0900

    feat: 스터디 노트 목록 컴포넌트 useStudyNotes 훅 연동 (#117)

--- src/components/studygroup-detail/StudyNoteList.tsx
+++ src/components/studygroup-detail/StudyNoteList.tsx
@@ -1,4 +1,6 @@
 import { Button, Card } from '@/components/common'
+import { useStudyNotes } from '@/hooks/study-note'
+import { format } from 'date-fns'
 import { Pencil, UserRound } from 'lucide-react'
 import { useNavigate } from 'react-router'
 
@@ -8,6 +10,7 @@ interface StudyNoteListProps {
 
 export function StudyNoteList({ groupId }: StudyNoteListProps) {
   const navigate = useNavigate()
+  const { data } = useStudyNotes(groupId)
 
   const handleCreate = () => {
     navigate(`/study-groups/${groupId}/notes/create`)
@@ -16,6 +19,8 @@ export function StudyNoteList({ groupId }: StudyNoteListProps) {
   const handleNoteDetail = (noteId: number) => {
     navigate(`/study-groups/${groupId}/notes/${noteId}`)
   }
+
+  const notes = data?.results ?? []
 
   return (
     <section className="border-custom-gray-200 flex flex-col gap-4 rounded-xl border p-6">
@@ -30,35 +35,44 @@ export function StudyNoteList({ groupId }: StudyNoteListProps) {
           <span>작성하기</span>
         </Button>
       </div>
-      <ul>
-        <li>
-          <button
-            type="button"
-            // API 연동 시 note.id로 변경
-            onClick={() => handleNoteDetail(1)}
-            className="w-full"
-          >
-            <Card className="p-4">
-              <div className="flex justify-between pb-3">
-                <p className="text-custom-gray-900 text-lg">
-                  React Hooks 실습 정리
-                </p>
-                <span className="text-custom-gray-500 text-sm">
-                  2024. 02. 16. 오전 05:30
-                </span>
-              </div>
-              <div className="flex items-center gap-3 py-[2px]">
-                <span className="bg-primary-100 centralize h-8 w-8 rounded-full">
-                  <UserRound className="text-primary-600 h-5 w-5" />
-                </span>
-                <span className="text-custom-gray-700 text-sm font-medium">
-                  김개발
-                </span>
-              </div>
-            </Card>
-          </button>
-        </li>
-      </ul>
+
+      {notes.length > 0 ? (
+        <ul>
+          {notes.map((note) => (
+            <li key={note.id}>
+              <button
+                type="button"
+                onClick={() => handleNoteDetail(note.id)}
+                className="w-full"
+              >
+                <Card className="p-4">
+                  <div className="flex justify-between pb-3">
+                    <p className="text-custom-gray-900 text-lg">{note.title}</p>
+                    <span className="text-custom-gray-500 text-sm">
+                      {format(new Date(note.created_at), 'yyyy. MM. dd.')}
+                    </span>
+                  </div>
+                  <div className="flex items-center gap-3 py-[2px]">
+                    <span className="bg-primary-100 centralize h-8 w-8 rounded-full">
+                      <UserRound className="text-primary-600 h-5 w-5" />
+                    </span>
+                    <span className="text-custom-gray-700 text-sm font-medium">
+                      {note.author.nickname}
+                    </span>
+                  </div>
+                </Card>
+              </button>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div className="centralize min-h-[194px] flex-col gap-2">
+          <p className="text-custom-gray-900 text-lg font-medium">
+            아직 작성된 스터디 기록이 없습니다.
+          </p>
+          <p className="text-custom-gray-600">첫 스터디 기록을 작성해보세요!</p>
+        </div>
+      )}
     </section>
   )
 }
