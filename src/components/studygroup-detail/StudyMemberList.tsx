da1429f73814849672071e9f4701e5433d30f6a0
hyebinpark <hoonywhitesocks@gmail.com>
2025-12-11 16:34:05 +0900

    feat: 리더 액션 버튼 ui 추가 (#141)

--- src/components/studygroup-detail/StudyMemberList.tsx
+++ src/components/studygroup-detail/StudyMemberList.tsx
@@ -1,5 +1,5 @@
 import { Badge, Card } from '@/components/common'
-import { UserRound } from 'lucide-react'
+import { Plus, UserRound, X } from 'lucide-react'
 
 export function StudyMemberList() {
   return (
@@ -8,13 +8,34 @@ export function StudyMemberList() {
         <p className="text-lg font-semibold">멤버 목록</p>
         <span className="text-custom-gray-500 text-sm">8명</span>
       </div>
+
       <ul className="flex max-h-[384px] flex-col gap-3 overflow-y-auto">
-        <li className="flex items-center gap-3">
-          <span className="bg-primary-100 centralize h-10 w-10 rounded-full">
-            <UserRound className="text-primary-600 h-5 w-5" />
-          </span>
-          <span>김개발</span>
-          <Badge className="h-6 rounded-sm">리더</Badge>
+        <li className="group flex justify-between">
+          <div className="flex items-center gap-3">
+            <span className="bg-primary-100 centralize h-10 w-10 rounded-full">
+              <UserRound className="text-primary-600 h-5 w-5" />
+            </span>
+            <span>김개발</span>
+            <Badge className="h-6 rounded-sm">리더</Badge>
+          </div>
+
+          {/* 리더 액션 버튼 */}
+          <div className="hidden items-center gap-3 text-sm group-hover:flex">
+            <button
+              type="button"
+              title="리더 위임"
+              className="text-primary-600 centralize bg-primary-100 h-7 w-7 rounded-full"
+            >
+              <Plus className="h-5 w-5" />
+            </button>
+            <button
+              type="button"
+              title="김개발님을 추방"
+              className="text-danger-600 centralize h-7 w-7 rounded-full bg-[#FEF2F2]"
+            >
+              <X className="h-5 w-5" />
+            </button>
+          </div>
         </li>
       </ul>
     </Card>
