a5cf88ea2b0bbcf52498b6a8c5735dce18b67b7f
Jay <jiyeyoun@outlook.com>
2025-12-12 15:22:35 +0900

    style: 마크다운에디터 화면 크기에 따른 ui 조정 (#149)

--- src/components/markdown/MarkdownExample.tsx
+++ src/components/markdown/MarkdownExample.tsx
@@ -2,7 +2,7 @@ export function MarkdownExample() {
   return (
     <div className="text-custom-gray-600 border-custom-gray-200 bg-custom-gray-50 flex h-18 flex-col justify-center gap-2 rounded-b-lg border-t px-4 text-xs font-medium sm:h-12 md:h-8 md:flex-row md:items-center md:justify-start">
       <p className="font-normal">마크다운 문법을 사용할 수 있습니다.</p>
-      <div className="flex flex-col gap-0.5 sm:flex-row md:gap-2">
+      <div className="xs:text-xs flex flex-col gap-0.5 text-[10px] sm:flex-row md:gap-2">
         <div className="flex gap-2">
           <span>**굵게**</span>
           <span>*기울임*</span>
