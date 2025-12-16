0d395e7f4ea1e91f484e1083edce01a6804d4b02
hyebinpark <hoonywhitesocks@gmail.com>
2025-12-16 16:46:09 +0900

    feat: withCredentials 설정 추가 (#190)

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -5,6 +5,7 @@ const IS_DEV = import.meta.env.MODE === 'development'
 
 export const axiosInstance = axios.create({
   baseURL: IS_DEV ? '' : API_BASE_URL,
+  withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   },
