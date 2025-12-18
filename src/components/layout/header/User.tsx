40ec2f9d42bbb362bc8d2e2f214a48a471c2575d
Jay <jiyeyoun@outlook.com>
2025-12-18 11:07:47 +0900

    chore: 외부 리다이렉트 링크 상수화 (#212)

--- src/components/layout/header/User.tsx
+++ src/components/layout/header/User.tsx
@@ -1,4 +1,5 @@
 import { UserModal } from '@/components/layout'
+import { EXTERNAL_LINKS } from '@/constants'
 import { useUserData } from '@/hooks'
 import { BellIcon, ChevronDown, ChevronUp, UserRound } from 'lucide-react'
 import { useState } from 'react'
@@ -14,7 +15,10 @@ export function User() {
     <div className="ml-auto flex">
       <div className="text-custom-gray-700 flex items-center gap-8 text-base">
         <div className="hidden md:flex md:gap-8">
-          <a href="" className="hover:text-primary-600 cursor-pointer">
+          <a
+            href={EXTERNAL_LINKS.LECTURES}
+            className="hover:text-primary-600 cursor-pointer"
+          >
             강의 목록
           </a>
           {/* 클릭하면 강의목록 페이지 렌더링 */}
@@ -22,7 +26,10 @@ export function User() {
             스터디 그룹
           </a>
           {/* 클릭하면 스터디그룹 페이지로 렌더링 */}
-          <a href="" className="hover:text-primary-600 cursor-pointer">
+          <a
+            href={EXTERNAL_LINKS.RECRUITMENT}
+            className="hover:text-primary-600 cursor-pointer"
+          >
             구인 공고
           </a>
           {/* 클릭하면 구인공고 페이지 렌더링 */}
